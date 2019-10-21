0307504bb17f1edd4f74a44a42fb2d161d09fb64
Lewis Stephens <rc0034542@student.riversidecollege.ac.uk>
2019-10-21 15:34:37 +0100

    completed it

--- assignment 2a/Form1.Designer.cs
+++ assignment 2a/Form1.Designer.cs
@@ -29,33 +29,27 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.R3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.R1 = new System.Windows.Forms.Label();
+            this.R2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::assignment_2a.Properties.Resources.circuit;
-            this.pictureBox1.Location = new System.Drawing.Point(107, 33);
+            this.pictureBox1.Location = new System.Drawing.Point(106, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(599, 393);
+            this.pictureBox1.Size = new System.Drawing.Size(616, 421);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(229, 222);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
             // 
             // button1
             // 
@@ -67,21 +61,23 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // label2
+            // R3
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(614, 378);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.R3.AutoSize = true;
+            this.R3.Location = new System.Drawing.Point(360, 376);
+            this.R3.Name = "R3";
+            this.R3.Size = new System.Drawing.Size(21, 13);
+            this.R3.TabIndex = 3;
+            this.R3.Text = "R3";
+            this.R3.Click += new System.EventHandler(this.Label2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(453, 222);
+            this.textBox1.Location = new System.Drawing.Point(458, 224);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "R2=";
             // 
             // textBox2
             // 
@@ -89,17 +85,56 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "R1=";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(324, 352);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "R3=";
+            // 
+            // R1
+            // 
+            this.R1.AutoSize = true;
+            this.R1.Location = new System.Drawing.Point(404, 74);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(21, 13);
+            this.R1.TabIndex = 8;
+            this.R1.Text = "R1";
+            // 
+            // R2
+            // 
+            this.R2.AutoSize = true;
+            this.R2.Location = new System.Drawing.Point(622, 263);
+            this.R2.Name = "R2";
+            this.R2.Size = new System.Drawing.Size(21, 13);
+            this.R2.TabIndex = 9;
+            this.R2.Text = "R2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(607, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(848, 610);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.R2);
+            this.Controls.Add(this.R1);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.R3);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -113,11 +148,14 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label R3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label R1;
+        private System.Windows.Forms.Label R2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- assignment 2a/Form1.cs
+++ assignment 2a/Form1.cs
@@ -18,9 +18,10 @@ namespace assignment_2a
         }
 
         private void Form1_Load(object sender, EventArgs e)
+       
         {
-
-        }
+     
+        } 
 
         private void PictureBox1_Click(object sender, EventArgs e)
         {
@@ -28,6 +29,46 @@ namespace assignment_2a
         }
 
         private void Button1_Click(object sender, EventArgs e)
+        {
+            double R1, R2, R3, RT;
+            
+            try
+            {
+                R1 = double.Parse(textBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect Input.");
+                R1 = 0.0;
+            }
+            try
+            {
+                R2 = double.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect Input.");
+                R2 = 0.0;
+            }
+            try
+            {
+                R3 = double.Parse(textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Incorrect Input.");
+                R3 = 0.0;
+            }
+            RT = R1 + R2 + R3;
+            label1.Text = "RT =" + RT;
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
         {
 
         }
